a5eed0fe74c5f8170f902c6a2ecf1a964b9b6edd
Fábio Lima da Silva <fabio.silva@crptecnologia.com.br>
2025-11-17 17:32:56 -0300

    Commit inicial

---
+++ EstudoOtel/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EstudoOtel.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstudoOtel.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
---
+++ EstudoOtel/Controllers/PessoaController.cs
@@ -0,0 +1,116 @@
+﻿using EstudoOtel.Contexts;
+using EstudoOtel.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstudoOtel.Controllers
+{
+    public class PessoaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PessoaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pessoa
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pessoas.ToListAsync());
+        }
+
+        // GET: Pessoa/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var pessoa = await _context.Pessoas.FirstOrDefaultAsync(m => m.Id == id);
+            if (pessoa == null) return NotFound();
+
+            return View(pessoa);
+        }
+
+        // GET: Pessoa/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pessoa/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome,Email")] Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pessoa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pessoa);
+        }
+
+        // GET: Pessoa/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var pessoa = await _context.Pessoas.FindAsync(id);
+            if (pessoa == null) return NotFound();
+
+            return View(pessoa);
+        }
+
+        // POST: Pessoa/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Email")] Pessoa pessoa)
+        {
+            if (id != pessoa.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pessoa);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PessoaExists(pessoa.Id)) return NotFound();
+                    else throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pessoa);
+        }
+
+        // GET: Pessoa/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var pessoa = await _context.Pessoas.FirstOrDefaultAsync(m => m.Id == id);
+            if (pessoa == null) return NotFound();
+
+            return View(pessoa);
+        }
+
+        // POST: Pessoa/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pessoa = await _context.Pessoas.FindAsync(id);
+            _context.Pessoas.Remove(pessoa);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PessoaExists(int id)
+        {
+            return _context.Pessoas.Any(e => e.Id == id);
+        }
+    }
+}
